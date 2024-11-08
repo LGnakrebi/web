a1e313a18f3d13970d628235e73b083dc8939243
LGnakrebi <160432635+LGnakrebi@users.noreply.github.com>
2024-11-08 16:23:33 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -253,7 +253,7 @@ jQuery(document).ready(function($) {
 
 	var siteCountDown = function() {
 
-		$('#date-countdown, #date-countdown2').countdown('2024/11/8', function(event) {
+		$('#date-countdown, #date-countdown2').countdown('2024/11/9', function(event) {
 		  var $this = $(this).html(event.strftime(''
 		  
 		    + '<span class="countdown-block"><span class="label">%d</span> days </span>'
